18c1e9b8d4e01a2c380beb7bdb2c249c14a74bbd
NirjalaP <nirjala.prajapati11@gmail.com>
2025-04-20 22:31:06 -0700

    Set up Vue app

---
+++ my-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app">
+    <img src="./assets/logo.png" alt="Vue logo">
+    <MessageComponent msg="this is my first Vue app!"/>
+  </div>
+</template>
+
+<script>
+import MessageComponent from './components/MessageComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MessageComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
